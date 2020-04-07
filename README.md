0d1debf47e205b25622e9065a24dcb64341fae29
Jacob Shtabnoy <shtabnoyjacob@scps.net>
2020-04-07 16:06:18 -0400

    Initialize repository
    Fill the repository with a simple base python project.
    
    Signed-off-by: Jacob Shtabnoy <shtabnoyjacob@scps.net>

---
+++ README.md
@@ -0,0 +1,24 @@
+# PyCCS
+
+**P**ython **C**lassi**C**ube **S**erver is a simple, cross-platform
+and extendable server for [ClassiCube](http://www.classicube.net).
+
+## To-do
+
+This project is still in its early stages, and the following things
+need to be worked on.
+
+- [ ] Basic Server System
+- [ ] Base Classic Protocol Support
+- [ ] Full CPE Support
+- [ ] Multiple Map Support
+- [ ] Plugin API
+- [ ] Base Plugins
+
+## Installation
+
+PyCCS is intended to be installed via pip, and if you would like,
+you may do so by doing:
+```shell script
+$ pip install pyccs
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+#  Copyright 2020 Jacob Shtabnoy <shtabnoyjacob@scps.net>
+#  This source code file is available under the terms of the ISC License.
+#  If the LICENSE file was not provided, you can find the full text of the license here:
+#  https://opensource.org/licenses/ISC
+
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="PyCCS",
+    version="0.0.0",
+    author="Jacob Shtabnoy",
+    author_email="shtabnoyjacob@scps.net",
+    description="A simple and extendable ClassiCube server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jshtab/PyCCS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
