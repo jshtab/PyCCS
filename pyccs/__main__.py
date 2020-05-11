76b5be48504b8a84535b39ca37bd758600beb38b
Jacob Shtabnoy <shtabnoyjacob@scps.net>
2020-05-11 15:12:26 -0400

    args: add command-line arguments to the main script
    Adds command line arguments to the main script, allowing you
    to easily customize some aspects of your server without
    having to write a python script.
    
    The name, message of the day, port, maxmimum player count,
    and name verification can all be modified from the command
    line.
    
    Soon: configuration files for multiple servers & logging
    improvements
    
    Signed-off-by: Jacob Shtabnoy <shtabnoyjacob@scps.net>

--- pyccs/__main__.py
+++ pyccs/__main__.py
@@ -1,11 +1,29 @@
 import logging
 import signal
+import argparse
+import configparser
 
 from pyccs import Server
+from pyccs.constants import VERSION
 import pyccs.plugin.livewire as LiveWire
 import pyccs.plugin.autocracy as Autocracy
 import pyccs.plugin.base as BasePlugin
 
+version = str(VERSION)
+
+parser = argparse.ArgumentParser(prog="pyccs",
+                                 description="A simple server for ClassiCube")
+parser.add_argument("-n", "--name", dest="server_name", type=str,
+                    default="PyCCS Server", help="Name of the server")
+parser.add_argument("-m", "--motd", dest="motd", type=str,
+                    default=version, help="Defines the message of the day")
+parser.add_argument("-l", "--level", dest="level_file", type=str,
+                    default="level.cw", help="What level the server should use")
+parser.add_argument("-P", "--port", dest="port", type=int,
+                    default=25565, help="The port the server will listen on")
+parser.add_argument("-p", "--players", dest="player_count", type=int,
+                    default=9, help="Maximum number of players")
+parser.add_argument("--no-verify", dest="verify_names", action="store_false", help="Disables name verification")
 
 def setup_logger():
     logger = logging.getLogger('PyCCS')
@@ -28,7 +46,9 @@ def setup_signals(server: Server):
 
 
 if __name__ == "__main__":
-    server = Server(max_players=2, verify_names=False, logger=setup_logger())
+    args = parser.parse_args()
+    server = Server(name=args.server_name, motd=args.motd, port=args.port, level=args.level_file,
+                    max_players=args.player_count, verify_names=args.verify_names, logger=setup_logger())
     server.add_plugin(BasePlugin)
     server.add_plugin(LiveWire)
     server.add_plugin(Autocracy)
